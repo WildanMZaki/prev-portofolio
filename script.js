2bc8a41e4c19e676e274702dc3ea23f725b8299f
WildanMZaki <wildanmzaki7@gmail.com>
2023-04-25 18:00:52 +0700

    third commit

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ function getData(url, callback) {
 }
 
 function showSkill(){
+    $('#hSkillsTitle').html("Hard Skills");
+    $('[data-item="all"]').addClass('active');
+    $('#mySkills').html('');        
     getData("./skills.json", data => {
-        data.forEach((el, ind) => {
+        data.forEach((el, ind) => {            
             // Cek apakah punya value atau child
             let value;
             if (el.hasOwnProperty("value")) {
@@ -29,9 +32,37 @@ function showSkill(){
                             <small class="text-warning more-skill" data-index="${ind}">Show more..</small>
                         </div>`: ''}
                 </div>
-            `)
+            `);
         });
     })
 }
 
 showSkill();
+
+$('#mySkills').on('click', '.more-skill', (e) => {
+     getData("./skills.json", data => {
+        let selected = data[parseInt(e.currentTarget.dataset.index)];
+        $('#hSkillsTitle').html(selected.name);
+        $('#hardSkilBreadcrumb').append(`<li class="breadcrumb-item active" data-item="sub">${selected.name}</li>`);
+        $('[data-item="all"]').removeClass('active');
+        $('#mySkills').html('');
+        selected.childs.forEach(el => {
+            $('#mySkills').append(`
+                <div class="skill-item my-2">
+                    <div class="d-flex justify-content-between">
+                        <h5>${el.name}</h5>
+                        <h5>${el.value} %</h5>
+                    </div>
+                    <div class="w-100 bg-light rounded-pill">
+                        <div class="w${el.value} bg-warning text-warning rounded-pill">.</div>
+                    </div>
+                </div>
+            `);
+        })
+     })
+});
+
+$('[data-item="all"]').click(e => {
+    $('[data-item="sub"]').remove();
+    showSkill();    
+});
